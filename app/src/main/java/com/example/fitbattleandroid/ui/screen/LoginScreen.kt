3473e7ce629ea063d14e4895499869947903cb38
ko-tarou <akokoa1221@gmail.com>
2024-10-26 12:53:27 +0900

    LoginScreenの作成

---
+++ app/src/main/java/com/example/fitbattleandroid/ui/screen/LoginScreen.kt
@@ -0,0 +1,119 @@
+package com.websarva.wings.android.myapplication
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import org.example.project.ui.common.CommonOutlinedTextField
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LoginScreen(navController: NavController) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier =
+        Modifier
+            .fillMaxSize()
+            .background(Color.DarkGray)
+    ) {
+        Column(
+            modifier =
+            Modifier
+                .clip(RoundedCornerShape(16.dp))
+                .background(Color.Black.copy(alpha = 0.4f))
+                .padding(200.dp)
+        ) {
+        }
+
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier =
+            Modifier
+                .fillMaxSize()
+                .imePadding()
+                .padding(16.dp)
+        ) {
+            Text(
+                text = "ログイン",
+                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.Bold),
+                color = Color.White
+            )
+
+            CommonOutlinedTextField(
+                value = email,
+                label = "メールアドレス",
+                onValueChange = { email = it }
+            )
+            CommonOutlinedTextField(
+                value = password,
+                label = "パスワード",
+                onValueChange = { password = it }
+
+            )
+
+            Button(
+                onClick = { navController.navigate("top_screen") },
+                shape = RoundedCornerShape(20.dp),
+                colors =
+                ButtonDefaults.buttonColors(
+                    containerColor = Color.Gray
+                ),
+                modifier =
+                Modifier
+                    .width(200.dp)
+            ) {
+                Text(
+                    text = "ログイン",
+                    color = Color.White
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CommonOutlineTextFieldPreview() {
+    var email by remember { mutableStateOf("") }
+
+    CommonOutlinedTextField(
+        label = "メールアドレス",
+        value = email,
+        onValueChange = { email = it }
+    )
+}
+
+@Composable
+@Preview
+fun LoginScreenPreview() {
+    LoginScreen(navController = rememberNavController())
+}
