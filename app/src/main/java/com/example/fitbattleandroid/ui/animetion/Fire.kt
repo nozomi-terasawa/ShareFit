a0bcb33354f68eac0930bbadf0adaa5f358564dd
ko-tarou <akokoa1221@gmail.com>
2024-10-27 00:12:59 +0900

    Fireのanimetion作成

--- app/src/main/java/com/example/fitbattleandroid/ui/animetion/Fire.kt
+++ app/src/main/java/com/example/fitbattleandroid/ui/animetion/Fire.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -24,10 +25,21 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.fitbattleandroid.R
 import kotlinx.coroutines.delay
+import kotlin.math.cos
+import kotlin.math.sin
 import kotlin.random.Random
 
 @Composable
 fun FireAnimeModule()  {
+
+    val fireParticles = remember { mutableStateListOf<FireParticleData>() }
+
+    LaunchedEffect(Unit) {
+        while(true){
+            fireParticles.add(FireParticleData())
+            delay(100)
+        }
+    }
 
     Box(
         contentAlignment = Alignment.Center,
@@ -36,47 +48,41 @@ fun FireAnimeModule()  {
             .fillMaxSize()
             .background(Color.DarkGray),
     ) {
-        Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier =
-            Modifier
-                .fillMaxSize(),
-        ) {
-            val image = painterResource(R.drawable.pngtreeburning_fire_5637806)
 
-            Column{
-//                Image(
-//                    painter = image,
-//                    contentDescription = null,
-//                    modifier = Modifier
-//                        .size(100.dp)
-//                )
-                for(i in 0 until 10){
-                    FireParticle(image)
-                }
+        val image = painterResource(R.drawable.pngtreeburning_fire_5637806)
+        val fire = painterResource(R.drawable.mainfire)
+
+
+        Image(
+            painter = fire,
+            contentDescription = null,
+            modifier = Modifier.size(100.dp)
+        )
+
+        fireParticles.forEach { particle ->
+            FireParticle(image = image,data = particle){
+                fireParticles.remove(particle)
             }
         }
     }
 }
 
 @Composable
-fun FireParticle(image: Painter) {
+fun FireParticle(image: androidx.compose.ui.graphics.painter.Painter,data: FireParticleData,onDisapper:() -> Unit) {
 
-    val angle = Random.nextDouble(0.0,2*Math.PI)
-    val speed = Random.nextFloat() * 5 + 2
     var offsetX by remember { mutableStateOf(0f) }
     var offsetY by remember { mutableStateOf(0f) }
     var alpha by remember { mutableStateOf(1f) }
 
     LaunchedEffect(Unit) {
         while (alpha > 0f) {
-            offsetX += Random.nextFloat() * 10 - 5
-            offsetY += Random.nextFloat() * 10 - 5
-            alpha -= 0.05f
+            offsetX += (cos(data.angle)*data.speed).toFloat()
+            offsetY += (sin(data.angle)*data.speed).toFloat()+1
+            alpha -= 0.02f
 
             delay(50)
         }
+        onDisapper()
     }
 
     Image(
@@ -85,8 +91,8 @@ fun FireParticle(image: Painter) {
         modifier = Modifier
             .size(5.dp)
             .graphicsLayer(
-                translationX = offsetX+100f,
-                translationY = offsetY-200f,
+                translationX = offsetX,
+                translationY = offsetY-50f,
                 scaleX = 1f,
                 scaleY = 1f,
                 alpha = alpha
@@ -94,6 +100,10 @@ fun FireParticle(image: Painter) {
     )
 }
 
+data class FireParticleData(
+    val angle:Double = Random.nextDouble(0.0,2*Math.PI),
+    val speed:Float = Random.nextFloat()*5+2
+)
 
 @Preview
 @Composable
