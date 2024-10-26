086303dc75cd3384a8c1d7878ef135bcf0a5281b
yuukikounishi <happywestyuki@gmail.com>
2024-10-27 00:41:51 +0900

    fitnessmemoryscreenの作成

---
+++ app/src/main/java/com/example/fitbattleandroid/ui/common/CountdownTo24.kt
@@ -0,0 +1,70 @@
+package com.example.fitbattleandroid.ui.common
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun ShowCurrentTimeAndRemainingTime(modifier: Modifier) {
+    var currentTime by remember { mutableStateOf(LocalDateTime.now()) }
+
+    // 毎秒現在時刻を更新する
+    LaunchedEffect(Unit) {
+        while (true) {
+            currentTime = LocalDateTime.now() // 現在時刻を取得
+            kotlinx.coroutines.delay(1000) // 1秒待機
+        }
+    }
+
+    val endOfDay = currentTime.toLocalDate().atStartOfDay().plusDays(1) // 24時の時刻を取得
+    val remainingSeconds = ChronoUnit.SECONDS.between(currentTime, endOfDay) // 残り秒数を計算
+
+    val currentHour = currentTime.hour
+    val currentMinute = currentTime.minute
+    val currentSecond = currentTime.second
+
+    val hours = remainingSeconds / 3600 // 残り時間の時間
+    val minutes = (remainingSeconds % 3600) / 60 // 残り時間の分
+    val seconds = remainingSeconds % 60 // 残り時間の秒
+
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // 現在の時刻を表示
+//        Text(
+//            text = "現在の時刻: $currentHour:$currentMinute:$currentSecond",
+//            fontSize = 30.sp,
+//            fontWeight = FontWeight.Bold,
+//            color = Color.Black,
+//        )
+
+        // 残り時間を表示
+        Text(
+            text = String.format("リセットまで %02d:%02d:%02d", hours, minutes, seconds),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+        )
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Preview(showBackground = true)
+@Composable
+fun ShowCurrentTimeAndRemainingTimePreview() {
+    ShowCurrentTimeAndRemainingTime(modifier = Modifier)
+}
---
+++ app/src/main/java/com/example/fitbattleandroid/ui/screen/FitnessMemoryScreen.kt
@@ -0,0 +1,193 @@
+package com.example.fitbattleandroid.ui.screen
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.fitbattleandroid.R
+import com.example.fitbattleandroid.ui.common.ShowCurrentTimeAndRemainingTime
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun FitnessMemory(modifier: Modifier) {
+
+    var calories by remember { mutableStateOf(("250")) }
+    Column(
+        modifier = Modifier
+            .fillMaxSize(), // 画面全体を使用
+        verticalArrangement = Arrangement.Center, // 子要素を上から配置
+        horizontalAlignment = Alignment.CenterHorizontally // 子要素を左揃え
+    ) {
+        Text(
+            text = "消費カロリー",
+            fontSize = 22.sp, // 大きなフォントサイズ
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+        )
+        Text(
+            text = "today", // ヘッダーのテキスト
+            fontSize = 28.sp, // 大きなフォントサイズ
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+
+            modifier = Modifier
+                .padding(top = 100.dp) // ヘッダーの上部に余白を追加
+
+        )
+        CaloriMeter(
+            modifier = Modifier,
+            max = 2000f,
+            progress = calories.toFloat()
+            //.padding(90.dp)
+        )
+
+        ShowCurrentTimeAndRemainingTime(modifier)
+
+        Text(
+            modifier = Modifier
+                .padding(top = 10.dp)
+                .align(Alignment.CenterHorizontally),
+            text = "$calories kcal",
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+        )
+    }
+}
+
+
+@Composable
+fun CaloriMeter(modifier: Modifier, max: Float, progress: Float) {
+    val circleAngle = 360f
+    val angle = 240f
+    val progressWidth = 24.dp
+    val backgroundWidth = 24.dp
+    val startAngle = (circleAngle / 4) + ((circleAngle - angle) / 2)
+
+    Box(modifier = Modifier
+        .padding(top = 100.dp, bottom = 100.dp),
+        contentAlignment = Alignment.Center
+    ) {
+
+        Canvas(
+            modifier =
+            Modifier
+                .size(250.dp)
+                .padding(10.dp),
+            onDraw = {
+                // 外枠を描画
+                drawArc(
+                    color = Color.Black,
+                    startAngle = startAngle,
+                    sweepAngle = angle,
+                    useCenter = false,
+                    style =
+                    Stroke(
+                        width = backgroundWidth.toPx(),
+                        cap = StrokeCap.Round,
+                    ),
+                    size = Size(size.width, size.height),
+                    //topLeft = Offset(4f, 4f),// 少しずらして影にする
+                )
+                // 背景を描画
+                drawArc(
+                    brush = Brush.radialGradient(
+                        colors = listOf(Color(192, 192, 192), Color(127, 192, 188)),
+                        center = Offset(size.width / 2, size.height / 2),
+                        radius = size.width / 2
+                    ),
+                    startAngle = startAngle,
+                    sweepAngle = angle,
+                    useCenter = false,
+                    style = Stroke(width = backgroundWidth.toPx(), cap = StrokeCap.Round),
+                    size = Size(size.width, size.height),
+                )
+                // 進捗を描画
+                // 進捗部分にグラデーションをかけて立体的なエフェクト
+                drawArc(
+                    brush = Brush.sweepGradient(
+                        colors = listOf(Color(33, 108, 94), Color(80, 200, 180)),
+                        center = Offset(size.width / 2, size.height / 2),
+                    ),
+                    startAngle = startAngle,
+                    sweepAngle = angle / max * progress,
+                    useCenter = false,
+                    style = Stroke(width = progressWidth.toPx(), cap = StrokeCap.Round),
+                    size = Size(size.width, size.height),
+                )
+//                    // 光沢のハイライトを追加して円の上部に光が当たっているように見せる
+//                    drawArc(
+//                        brush = Brush.linearGradient(
+//                            colors = listOf(Color.White.copy(alpha = 0.3f), Color.Transparent),
+//                            start = Offset(size.width / 2, 0f),
+//                            end = Offset(size.width / 3, size.height / 2),
+//                        ),
+//                        startAngle = startAngle - 5, // ハイライトを調整
+//                        sweepAngle = angle / 1,
+//                        useCenter = false,
+//                        style = Stroke(width = progressWidth.toPx() / 2, cap = StrokeCap.Round),
+//                        size = Size(size.width, size.height),
+//                    )
+
+            },
+        )
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.pngburning_fire_5637806.png),
+                contentDescription = null,
+                modifier = Modifier.size(80.dp),
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Text(
+                text = "0/${max.toInt()}",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black,
+                modifier = Modifier.padding(top = 8.dp) // 画像の下に少しスペースを追加
+            )
+        }
+
+    }
+
+}
+
+
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Preview(showSystemUi = true)
+@Composable
+fun FitnessMemoryPreview() {
+    FitnessMemory(modifier = Modifier)
+}
