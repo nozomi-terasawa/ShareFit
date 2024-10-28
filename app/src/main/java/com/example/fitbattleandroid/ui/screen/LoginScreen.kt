411fe10dc9934d3e1374302b9fe98718b32e57c9
tera330 <tera877tech@gmail.com>
2024-10-29 04:08:24 +0900

    lintによるフォーマット修正

--- app/src/main/java/com/example/fitbattleandroid/ui/screen/LoginScreen.kt
+++ app/src/main/java/com/example/fitbattleandroid/ui/screen/LoginScreen.kt
@@ -1,33 +1,15 @@
 package com.example.fitbattleandroid.ui.screen
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.imePadding
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.fitbattleandroid.ui.common.Background
@@ -40,9 +22,6 @@ import com.example.fitbattleandroid.ui.common.NormalText
 import com.example.fitbattleandroid.ui.common.TitleText
 import com.example.fitbattleandroid.ui.common.TransparentBottom
 import com.example.fitbattleandroid.ui.navigation.Screen
-import com.example.fitbattleandroid.ui.theme.onPrimaryDark
-import com.example.fitbattleandroid.ui.theme.primaryContainerDarkMediumContrast
-import com.example.fitbattleandroid.ui.theme.primaryContainerLight
 
 @Composable
 fun LoginScreen(navController: NavController) {
@@ -52,7 +31,6 @@ fun LoginScreen(navController: NavController) {
     Background {
         Header {
             Body {
-
                 TitleText("ログイン")
 
                 Spacer(modifier = Modifier.size(50.dp))
@@ -73,8 +51,13 @@ fun LoginScreen(navController: NavController) {
                     NormalText("ログイン")
                 }
 
-                TransparentBottom({navController.navigate(Screen.Regi.route) { popUpTo(Screen.Login.route) { inclusive = true }
-                        launchSingleTop = true }}
+                TransparentBottom(
+                    {
+                        navController.navigate(Screen.Regi.route) {
+                            popUpTo(Screen.Login.route) { inclusive = true }
+                            launchSingleTop = true
+                        }
+                    },
                 ) {
                     MinText("新規登録の方はこちら")
                 }
--- app/src/main/java/com/example/fitbattleandroid/ui/screen/RegistrationScreen.kt
+++ app/src/main/java/com/example/fitbattleandroid/ui/screen/RegistrationScreen.kt
@@ -1,33 +1,15 @@
 package com.example.fitbattleandroid.ui.screen
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.imePadding
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.fitbattleandroid.ui.common.Background
@@ -40,9 +22,6 @@ import com.example.fitbattleandroid.ui.common.NormalText
 import com.example.fitbattleandroid.ui.common.TitleText
 import com.example.fitbattleandroid.ui.common.TransparentBottom
 import com.example.fitbattleandroid.ui.navigation.Screen
-import com.example.fitbattleandroid.ui.theme.onPrimaryDark
-import com.example.fitbattleandroid.ui.theme.primaryContainerDarkMediumContrast
-import com.example.fitbattleandroid.ui.theme.primaryContainerLight
 
 @Composable
 fun RegistrationScreen(
@@ -51,7 +30,7 @@ fun RegistrationScreen(
 ) {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
-    var username by remember { mutableStateOf( "") }
+    var username by remember { mutableStateOf("") }
 
     Background {
         Header {
@@ -78,14 +57,20 @@ fun RegistrationScreen(
                     onValueChange = { username = it },
                 )
 
-                NormalBottom(onClick = { navController.navigate("main") },
+                NormalBottom(
+                    onClick = { navController.navigate("main") },
                 ) {
                     NormalText("新規登録")
                 }
 
-                TransparentBottom({ navController.navigate(Screen.Login.route) { popUpTo(Screen.Regi.route) { inclusive = true }
-                            launchSingleTop = true }}
-                ){
+                TransparentBottom(
+                    {
+                        navController.navigate(Screen.Login.route) {
+                            popUpTo(Screen.Regi.route) { inclusive = true }
+                            launchSingleTop = true
+                        }
+                    },
+                ) {
                     MinText("登録済みの方はこちら")
                 }
             }
