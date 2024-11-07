1151b453b24fe5ad22b89c97446126e7c5567260
Fujikouki <c1202599@st.kanazawa-it.ac.jp>
2024-11-07 22:48:00 +0900

    headersのimportを変更

--- app/src/main/java/com/example/fitbattleandroid/data/EncounterRemoteDatasource.kt
+++ app/src/main/java/com/example/fitbattleandroid/data/EncounterRemoteDatasource.kt
@@ -6,12 +6,12 @@ import com.example.fitbattleandroid.data.remote.EntryGeoFenceRes
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.headers
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
 import io.ktor.http.contentType
-import io.ktor.http.headers
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
